48043a44818fd7593310c371647c5ede208b1bde
Khalilur Rahman <khal.rahman@gmail.com>
2016-05-02 00:02:50 +0800

    init commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    entry: "./index.js",
+    output: {
+        path: __dirname,
+        filename: "bundle/app.bundle.js"
+    },
+    module: {
+        loaders: [
+            { 
+                test: /.jsx?$/, 
+                loader: 'babel-loader', 
+                exclude: [/node_modules/],
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            },
+            { 
+                test: /\.css$/, 
+                loader: "style-loader!css-loader"
+            }
+        ]
+    }
+};
