6042264951d83de3a91d6dc79509cba3c4757f65
Khalilur Rahman <khal.rahman@gmail.com>
2016-05-02 23:05:19 +0800

    added react and react-dom and main compnt

---
+++ src/components/main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Component } from 'react';
+
+export default class MainApp extends Component {
+	render() {
+		return (
+			<div>
+				<h1>World's Weather</h1>
+			</div>	
+		);
+	}
+}
--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
-require('../css/style.css');
-document.write('It works');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import MainApp from './components/main';
+
+ReactDOM.render(
+	<MainApp />, document.querySelector('.container')
+);
